062c6c87baf04e759036e313e27bd6d74f6a77ec
Tasnimul116 <96897983+Tasnimul116@users.noreply.github.com>
2023-08-26 22:18:43 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Nav from "./components/Nav";
+import Jumbotron from "./components/jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+
 
 function App() {
 
   return (
     <div className="App">
-      Hello
-    </div>
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+      <WebgiViewer/>
+      </div>
   );
 }
 
